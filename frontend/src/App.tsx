5839d0cbfba3507a423db0613003c0b39a369e65
ajeetk7ev <ajeetk8568@gmail.com>
2026-03-14 10:45:04 +0530

    Initialize frontend with react, tailwindcss and shadcnui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+   
+
+  return (
+   <div>
+       <Button className='bg-green-400'>Click Here</Button>
+   </div>
+  )
+}
+
+export default App
